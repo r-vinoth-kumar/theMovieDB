ff71dd317066f715bf91022f0c11556348ecd094
Vinoth Kumar Ramalingam <rvk21011992@gmail.com>
2018-07-30 20:29:01 +0100

    Base Project Commit

---
+++ theMovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  theMovieDB
+//
+//  Created by Vinoth Kumar Ramalingam on 30/07/18.
+//  Copyright © 2018 Vinoth Kumar Ramalingam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

7b8f4715d263d5ac320865aaaf1beb260a5a946c
Vinoth Kumar Ramalingam <rvk21011992@gmail.com>
2018-08-04 23:46:55 +0100

    update base view controller to show error view (placeholder)

--- theMovieDB/View/BaseViewController.swift
+++ theMovieDB/View/BaseViewController.swift
@@ -10,26 +10,20 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
+    @IBOutlet var errorView: UIView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func showErrorWith(message: String, animated: Bool) {
     }
-    */
+
+    func hideError(animated: Bool) {
+    }
 
 }

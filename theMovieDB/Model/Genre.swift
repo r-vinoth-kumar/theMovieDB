fa66ac833f421b0baacbf57819a7097a238c25b6
Vinoth Kumar Ramalingam <rvk21011992@gmail.com>
2018-08-04 23:35:18 +0100

    created movie models

---
+++ theMovieDB/Model/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  theMovieDB
+//
+//  Created by Vinoth Kumar Ramalingam on 04/08/18.
+//  Copyright © 2018 Vinoth Kumar Ramalingam. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Codable {
+    var id: Int?
+    var name: String?
+}
---
+++ theMovieDB/Model/Movie.swift
@@ -0,0 +1,130 @@
+//
+//  Movie.swift
+//  theMovieDB
+//
+//  Created by Vinoth Kumar Ramalingam on 04/08/18.
+//  Copyright © 2018 Vinoth Kumar Ramalingam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Movie: Codable {
+    let id: Int
+    let title: String?
+    var overview: String?
+    var posterPath: String?
+    var voteAvarage: Decimal?
+    var voteCount: Int?
+    var popularity: Decimal?
+    let releaseDateString: String
+    var backdropPath: String?
+    var budget: Int?
+    var revenue: Int?
+    var runtime: Int?
+    var adult: Bool?
+    var genres: [Genre]?
+    var tagline: String?
+    var language: String?
+    var homepage: String?
+
+    var releaseDate: Date? {
+        get {
+            let dayTimeFormatter = DateFormatter()
+            dayTimeFormatter.dateFormat = "yyyy-MM-dd"
+            return dayTimeFormatter.date(from: self.releaseDateString)
+        }
+    }
+
+    var releaseYear: Int {
+        get {
+            let calendar = Calendar.current
+            if let releaseDate = self.releaseDate {
+                return calendar.component(.year, from: releaseDate)
+            } else {
+                return 0
+            }
+        }
+    }
+
+    var isTrending: Bool {
+        get {
+            return isNewRelease && popularity ?? 0 > 300.0
+        }
+    }
+
+    var isEditorsChoice: Bool {
+        get {
+            return voteAvarage ?? 0 > 7.0 && popularity ?? 0 > 250.0
+        }
+    }
+
+    var isNewRelease: Bool {
+        get {
+            let calendar = Calendar.current
+            if let releaseDate = self.releaseDate {
+                return calendar.dateComponents([.month], from: releaseDate, to: Date()).month ?? 6 < 6
+            } else {
+                return false
+            }
+        }
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case posterPath = "poster_path"
+        case voteAvarage = "vote_average"
+        case voteCount = "vote_count"
+        case popularity
+        case releaseDateString = "release_date"
+        case backdropPath = "backdrop_path"
+        case budget
+        case revenue
+        case runtime
+        case adult
+        case genres
+        case tagline
+        case language = "original_language"
+        case homepage
+    }
+
+    func loadPosterImage(success: @escaping (UIImage?) -> Void) -> Void {
+
+        if let path = posterPath {
+            //API service to get image
+        } else {
+            //default placeholder image
+            success(UIImage(named: "coverplaceholder"))
+        }
+    }
+
+    func loadBackdropImage(success: @escaping (UIImage?) -> Void) -> Void {
+
+        if let path = backdropPath {
+            //API service to get image
+        } else {
+            //default placeholder image
+            success(UIImage(named: "wideplaceholder"))
+        }
+    }
+
+
+}
+
+extension Movie : Equatable {
+    static func == (lhs: Movie, rhs: Movie) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    static func != (lhs: Movie, rhs: Movie) -> Bool {
+        return lhs.id != rhs.id
+    }
+}
+
+extension Movie : Hashable {
+    var hashValue: Int {
+        return id.hashValue
+    }
+}

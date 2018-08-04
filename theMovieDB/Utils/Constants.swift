dd45348baf38c22689371dd38b0051c6ba4188e1
Vinoth Kumar Ramalingam <rvk21011992@gmail.com>
2018-08-04 23:43:40 +0100

    added constant api parameter keys and values

--- theMovieDB/Utils/Constants.swift
+++ theMovieDB/Utils/Constants.swift
@@ -15,4 +15,18 @@ struct Constants {
         static let imageURL: String = "https://image.tmdb.org/t/p/w"
         static let apiKey: String = "80e9afea56635f17c97e6ed2fe8629b8"
     }
+
+    struct ParameterKey {
+        static let apiKey = "api_key"
+        static let sort = "sort_by"
+        static let query = "query"
+        static let page = "page"
+        static let id = "id"
+        static let includeAdult = "include_adult"
+    }
+
+    struct ParameterValue {
+        static let sortOrder = "popularity.desc"
+    }
+
 }
